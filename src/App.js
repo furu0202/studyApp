21de973c2df8eebf454eba04bcfb9d07ba3f0eb0
furu0202 <furuka.0202@gmail.com>
2023-04-30 02:36:09 +0900

    やり直し

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
+import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0);
-  const pushButtonPlus = () => {
-    setCount((prevcount) => prevcount + 1);
-  };
-  const pushButtonMinus = () => {
-    // if (count > 0) {
-    //   setCount((prevcount) => prevcount - 1);
-    // }
-    // setCount((prevcount) => (prevcount > 0 ? prevcount - 1 : 0));
-    setCount((prevcount) => prevcount - 1);
-  };
-
-  const pushButtonReset = () => {
-    setCount((prevcount) => (prevcount = 0));
-  };
-
   return (
     <div className='App'>
-      <p>{count}</p>
-      <button onClick={pushButtonPlus}>これは＋ボタンです</button>
-      <button onClick={pushButtonMinus} disabled={count === 0}>
-        これはーボタンです
-      </button>
-      <button onClick={pushButtonReset}>これはリセットボタンです</button>
+      <p>0</p>
+      <button>これはプラスボタンです</button>
     </div>
   );
 }
