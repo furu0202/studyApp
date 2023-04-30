f33be22d0e4633d1120e9d31d9a94c11525e490f
furu0202 <furuka.0202@gmail.com>
2023-04-30 23:49:24 +0900

    カウントアプリ・やり直し後

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const pushButtonPlus = () => {
+    setCount((prevcount) => prevcount + 1);
+  };
+  const pushButtonMinus = () => {
+    setCount((prevcount) => prevcount - 1);
+  };
+
+  const pushButtonReset = () => {
+    setCount((prevcount) => (prevcount = 0));
+  };
+
   return (
     <div className='App'>
-      <p>0</p>
-      <button>これはプラスボタンです</button>
+      <p>{count}</p>
+      <button onClick={pushButtonPlus}>＋ボタン</button>
+      <button onClick={pushButtonMinus} disabled={count === 0}>
+        ーボタン
+      </button>
+      <button onClick={pushButtonReset}>リセットボタン</button>
     </div>
   );
 }
