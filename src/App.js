a6463d2a8a9711c1ed211815143e967669c54ca9
furu0202 <furuka.0202@gmail.com>
2023-05-05 22:34:59 +0900

    テキストフォーム　ボタンの実装

--- src/App.js
+++ src/App.js
@@ -14,6 +14,14 @@ function App() {
     setCount((prevcount) => (prevcount = 0));
   };
 
+  const [counter, setCounter] = useState(0);
+  const textPushButtonPlus = () => {
+    setCounter((prevcount) => prevcount + 2);
+  };
+  const textPushButtonMinus = () => {
+    setCounter((prevcount) => prevcount - 2);
+  };
+
   return (
     <div className='App'>
       <p>{count}</p>
@@ -22,8 +30,12 @@ function App() {
         ーボタン
       </button>
       <button onClick={pushButtonReset}>リセットボタン</button>
-      <p>テキストフォーム</p>
+      <p>現在の入力数は{counter}です</p>
       <input type='text'></input>
+      <button onClick={textPushButtonPlus}>＋ボタン</button>
+      <button onClick={textPushButtonMinus} disabled={counter === 0}>
+        ーボタン
+      </button>
     </div>
   );
 }
